38cea32b1060ca7ad962c46eafa256802f7047fb
Bekbolot <justworking8888@gmail.com>
2024-06-22 17:55:20 +0600

    fds

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -5,6 +5,7 @@ import PizzaBlock from '../components/PizzaBlock';
 import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../App';
 import sorts from '../data/sort.json'
+import { Skeleton } from '../components/PizzaSceleton';
 
 
 const colors = [
@@ -14,14 +15,14 @@ function HomePage() {
     const { isDarkMode, toggleTheme } = useContext(ThemeContext)
     const [query, setQuery] = useState('')
     const [pizzas, setPizzas] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [status, setStatus] = useState('idle')
     const [curenCaategory, setCurenCaategory] = useState(1)
     const [curentSorts, setCurentSorts] = useState(1)
     const [color, setColor] = useState('green')
     const [curentColors, setCurentColors] = useState(0)
 
     const fetchPizzas = async () => {
-        setLoading(true)
+        setStatus('pending')
         const sort = sorts[curentSorts].value.replace('-', '')
         const oreder = sorts[curentSorts].value.includes('-') ? 'asc' : 'desc'
         const url = new URL(`https://6367b246edc85dbc84d9ba5d.mockapi.io/products`)
@@ -32,14 +33,16 @@ function HomePage() {
         try {
             const response = await fetch(url)
             if (!response.ok, response.status === 404) {
+                setStatus('error')
                 return new Error('ERROR')
+
             }
             const result = await response.json()
             setPizzas(result)
+            setStatus('succses')
         } catch (error) {
+            setStatus('error')
             return new Error('ERROR')
-        } finally {
-            setLoading(false)
         }
     }
 
@@ -47,7 +50,34 @@ function HomePage() {
         fetchPizzas()
     }, [curenCaategory, curentSorts, query])
 
-
+    
+    const pizzasComponent = {
+        succses:
+            pizzas?.map((item) =>
+                <PizzaBlock
+                    price={item.price}
+                    title={item.title}
+                    img={item.image}
+                    key={item._id} />),
+        pending:
+            <>
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+            </>,
+        idle: <>
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+        </>,
+        error: <>
+            <img src="https://web-promo.ua/wp-content/uploads/2023/06/e11044b915dc39afc3004430606bd6d1.jpg" alt="" />
+        </>
+    }
     return (
         <div
             style={isDarkMode ? { background: colors[curentColors] } : {}}
@@ -80,9 +110,7 @@ function HomePage() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {
-                            pizzas?.map((item) => <PizzaBlock price={item.price} title={item.title} img={item.image} key={item._id} />)
-                        }
+                        {pizzasComponent[status]}
                     </div>
                 </div>
             </div>
